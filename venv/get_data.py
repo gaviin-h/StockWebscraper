8b3677bae6b36c14bc7266656b59c28f490eda29
Gavin <ikgavh7@gmail.com>
2020-05-14 22:27:08 -0700

    New Data Formatting

--- venv/get_data.py
+++ venv/get_data.py
@@ -1,34 +1,43 @@
 
 import requests
 from bs4 import *
+import time
 import lxml
 
 # driver = webdriver.Chrome("Users/Gavin/Applications/Google\ Chrome.app")
 
 # Put all the tickers into a list
+result = open("data", "w")
 
 
 def get_tick(file):
+    x = []
     for i in file:
         i = i.replace("\n", "")
-        x = []
         x.append(i)
-        return x
+    return x
 
 
-def get_data(arr):  # Go through the list of tickers and get data
+def get_data(arr):  # Go through the list of tickers and get data, rests for 5 seconds after
     for j in range(0, len(arr)):
         tick = arr[j]
-        result = open("data", "w")
-
         url = "https://finance.yahoo.com/quote/" + tick + "?p=&.tsrc=fin-srch"
         data = requests.get(url).text
         soup = BeautifulSoup(data, 'lxml')
 
         price_box = soup.find('span', {'class': 'Trsdu(0.3s) Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(b)'})
         price = price_box.text
+        price_change = soup.find('span', {'data-reactid': '16'})
+        change = price_change.text
 
-        result.write(tick + " " + price)
+        result.write(tick + ' ' + price + ' ' + change + '\n')
+
+        """ DONT TOUCH THIS I NEED IT TO HELP PARSE THE HTML"""
+        """price_box = soup.find_all('span')
+        for x in price_box:
+            result.writelines(str(x) + "\n")"""
+
+        time.sleep(5)
 
         # open the html || should use helper methods
 
