1132fc7d1c49a5a4ce30ffd40d3c1cc62751e1cc
Gavin <ikgavh7@gmail.com>
2020-05-14 19:00:28 -0700

    Reorganized

--- venv/Main.py
+++ venv/Main.py
@@ -1,33 +1,6 @@
 # lets get some stock info!
 
-import requests
-import bs4 as bs
-
-# get the tickers names
-file = open("Tickers.txt", "r")
-x = []
-# driver = webdriver.Chrome("Users/Gavin/Applications/Google\ Chrome.app")
-
-# Put all the tickers into a list
-for i in file:
-    i = i.replace("\n", "")
-    x.append(i)
-
-def get_data(arr):  # Go through the list of tickers and get data
-    for j in range(0, len(x)):
-        tick = arr[j]
-        url = "https://finance.yahoo.com/quote/" + tick + "?p=&.tsrc=fin-srch"
-        data = requests.get(url).text
-        soup = bs(data.text, 'html.parser')
-        mydivs = soup.findAll('span', {"class": "Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)"})
-        print(mydivs)
-
-
-get_data(x)
-
-# open the html
-
-# parse the html for prices
+from get_data.py import get_data()
 
 # run an algorithm to find the hottest stock
 
---
+++ venv/get_data.py
@@ -0,0 +1,26 @@
+
+import requests
+import bs4 as bs
+
+# get the tickers names
+file = open("Tickers.txt", "r")
+x = []
+# driver = webdriver.Chrome("Users/Gavin/Applications/Google\ Chrome.app")
+
+# Put all the tickers into a list
+for i in file:
+    i = i.replace("\n", "")
+    x.append(i)
+
+
+def get_data(arr):  # Go through the list of tickers and get data
+    for j in range(0, len(x)):
+        tick = arr[j]
+        url = "https://finance.yahoo.com/quote/" + tick + "?p=&.tsrc=fin-srch"
+        data = requests.get(url).text
+        soup = bs(data.text, 'html.parser')
+        mydivs = soup.findAll('span', {"class": "Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)"})
+
+        # open the html || should use helper methods
+
+        # analyse the html and push the data to a new txt file called data.txt
